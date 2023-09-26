9cd64af36d15006fd9cd2c2d2f52303d285ee121
YmirBP <135678923+YmirBP@users.noreply.github.com>
2023-09-26 15:36:29 -0400

    Create Transaction.cs

---
+++ Entidades/Transaction.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int UserId { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
