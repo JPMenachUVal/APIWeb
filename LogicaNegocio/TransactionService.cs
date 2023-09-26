8e568ff8deaa7ed68269052a884b36854c207d33
Ian Martinez <145503631+bIacklol597@users.noreply.github.com>
2023-09-26 16:07:46 -0400

    Add files via upload

--- LogicaNegocio/TransactionService.cs
+++ LogicaNegocio/TransactionService.cs
@@ -5,33 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using DAL;
 using Entidades;
-
 namespace LogicaNegocio.Services
 {
     public class TransactionService
     {
         private readonly TransactionMng transactionMng;
-
         public TransactionService(string connectionString)
         {
             transactionMng = new TransactionMng(connectionString);
         }
-
         public List<Transaction> GetTransactions()
         {
             return transactionMng.GetTransactions();
         }
-
         public void AddTransaction(Transaction transaction)
-        {
-            // Aquí puedes realizar validaciones u operaciones adicionales antes de agregar la transacción.
+        { // Aquí puedes realizar validaciones u operaciones adicionales antes de agregar la transacción.
             transactionMng.AddTransaction(transaction);
         }
-
         public List<Transaction> GetTransactionsByDate(DateTime date)
         {
             return transactionMng.GetTransactionsByDate(date);
         }
-
     }
 }
