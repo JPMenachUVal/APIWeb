087b6135a715d78861e37d0ec24c88049cc430e3
Jpmc260802UV <103669140+JPMenachUVal@users.noreply.github.com>
2023-10-09 11:57:21 -0400

    API Web con MVC en la misma solución funcional - Menacho -- (9/10/2023)

---
+++ AppWeb/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using AppWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace AppWeb.Controllers
+{
+    public class PersonController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7248/api");
+        private readonly HttpClient _client;
+
+        public PersonController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<PersonViewModel> personList = new List<PersonViewModel>();
+            HttpResponseMessage response = await _client.GetAsync("/api/Person");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                personList = JsonConvert.DeserializeObject<List<PersonViewModel>>(data);
+            }
+            return View(personList);
+        }
+
+    }
+}
---
+++ AppWeb/Models/PersonViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AppWeb.Models
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
